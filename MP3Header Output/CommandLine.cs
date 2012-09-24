692e8716a9278f64d0a6b802098885e04062f50a
unknown <Sam@.(none)>
2012-09-23 20:25:03 -0700

    initial commit

---
+++ MP3Header Output/CommandLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class CommandLine
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Written by Sam Contapay");
+            Console.Out.WriteLine("This program takes in an argument that is the full path to an MP3 file and prints out the ID tag of the MP3 file.");
+            try
+            {
+                Mp3DisplayTag mp3Display = new Mp3DisplayTag(args[0]);
+                Console.Out.WriteLine(mp3Display.DisplayTags());
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.Out.WriteLine("The first argument can not be empty and must be the full path to the mp3 file. For example: 'c:\\path\\to\\my.mp3'");
+            }
+            catch (System.IO.FileNotFoundException e)
+            {
+                Console.Out.WriteLine("Unable to find the file designated by the path " + args[0] + ". Please make sure the file exists and try again.");
+            }
+            catch (TagLib.UnsupportedFormatException e)
+            {
+                Console.Out.WriteLine("Unsupported file format please make sure it is an MP3 file and try again.");
+            }
+        }
+    }
+}
---
+++ MP3Header Output/Mp3DisplayTag.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Mp3DisplayTag
+    {
+        public String mp3FileName { get; set; }
+        private TagLib.File tagfile = null;
+
+        public Mp3DisplayTag(String mp3filename) 
+        {
+            this.mp3FileName = mp3filename;
+            Console.Out.WriteLine("MP3 file set to: " + this.mp3FileName);
+
+            if ( ! System.IO.File.Exists( this.mp3FileName ) )
+                throw new System.IO.FileNotFoundException();
+
+            try
+            {
+                tagfile = TagLib.File.Create(this.mp3FileName);
+            }
+            catch (TagLib.UnsupportedFormatException e)
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Display the tags for the MP3 file. This will print to the console in a line break way the tags and their values of the MP3 file.
+        /// </summary>
+        /// <returns>A string with linebreaks that have the tag and their values for the ID3 tag.</returns>
+        public String DisplayTags()
+        {
+            StringBuilder tags = new StringBuilder();
+            tags.AppendLine("--------------TAGS-----------------");
+
+            tags.AppendLine("Tags on disk:   " + tagfile.TagTypesOnDisk);
+            tags.AppendLine("Tags in object: " + tagfile.TagTypes);
+            tags.AppendLine(String.Empty);
+
+            tags.AppendLine("Grouping: " + tagfile.Tag.Grouping);
+            tags.AppendLine("Title: " + tagfile.Tag.Title);
+            tags.AppendLine("TitleSort: " + tagfile.Tag.TitleSort);
+            tags.AppendLine("Album Artists:" + String.Join(",", tagfile.Tag.AlbumArtists));
+            tags.AppendLine("Album Artists Sort: " + String.Join(",", tagfile.Tag.AlbumArtistsSort));
+            tags.AppendLine("Performers: " + String.Join(",",tagfile.Tag.Performers));
+            tags.AppendLine("Performers Sort: " + String.Join(",", tagfile.Tag.PerformersSort));
+            tags.AppendLine("Composers: " + String.Join(",",tagfile.Tag.Composers));
+            tags.AppendLine("Composers Sort: " + String.Join(",",tagfile.Tag.ComposersSort));
+            tags.AppendLine("Conductor: " + tagfile.Tag.Conductor);
+            tags.AppendLine("Album: " + tagfile.Tag.Album);
+            tags.AppendLine("Album Sort: " + tagfile.Tag.AlbumSort);
+            tags.AppendLine("Comment: " + tagfile.Tag.Comment);
+            tags.AppendLine("Copyright: " + tagfile.Tag.Copyright);
+            tags.AppendLine("Genres: " + String.Join(",",tagfile.Tag.Genres));
+            tags.AppendLine("BPM: " + tagfile.Tag.BeatsPerMinute);
+            tags.AppendLine("Year: " + tagfile.Tag.Year);
+            tags.AppendLine("Track: " + tagfile.Tag.Track);
+            tags.AppendLine("TrackCount: " + tagfile.Tag.TrackCount);
+            tags.AppendLine("Disc: " + tagfile.Tag.Disc);
+            tags.AppendLine("DiscCount: " + tagfile.Tag.DiscCount);
+
+            tags.AppendLine("Lyrics: " + tagfile.Tag.Lyrics);
+            tags.AppendLine("Media Types:     " + tagfile.Properties.MediaTypes);
+
+            foreach (TagLib.ICodec codec in tagfile.Properties.Codecs)
+            {
+                TagLib.IAudioCodec acodec = codec as TagLib.IAudioCodec;
+                TagLib.IVideoCodec vcodec = codec as TagLib.IVideoCodec;
+
+                if (acodec != null && (acodec.MediaTypes & TagLib.MediaTypes.Audio) != TagLib.MediaTypes.None)
+                {
+                    tags.AppendLine("Audio Properties : " + acodec.Description);
+                    tags.AppendLine("Bitrate:    " + acodec.AudioBitrate);
+                    tags.AppendLine("SampleRate: " + acodec.AudioSampleRate);
+                    tags.AppendLine("Channels:   " + acodec.AudioChannels);
+                }
+
+                if (vcodec != null && (vcodec.MediaTypes & TagLib.MediaTypes.Video) != TagLib.MediaTypes.None)
+                {
+                    tags.AppendLine("Video Properties : " + vcodec.Description);
+                    tags.AppendLine("Width:      " + vcodec.VideoWidth);
+                    tags.AppendLine("Height:     " + vcodec.VideoHeight);
+                }
+            }
+
+            if (tagfile.Properties.MediaTypes != TagLib.MediaTypes.None)
+                tags.AppendLine("Length:     " + tagfile.Properties.Duration);
+
+            TagLib.IPicture[] pictures = tagfile.Tag.Pictures;
+
+            tags.AppendLine("Embedded Pictures: " + pictures.Length);
+
+            foreach (TagLib.IPicture picture in pictures)
+            {
+                tags.AppendLine(picture.Description);
+                tags.AppendLine("   MimeType: " + picture.MimeType);
+                tags.AppendLine("   Size:     " + picture.Data.Count);
+                tags.AppendLine("   Type:     " + picture.Type);
+            }
+
+            tags.AppendLine("--------------END------------------");
+
+            return tags.ToString();
+        }
+
+    }
+}
